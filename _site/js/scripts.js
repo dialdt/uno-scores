9073cc240546b05fcedbef0b145c80c3c99d5b88
Isham Jassat <isham.jassat@gmail.com>
2020-05-16 18:30:05 +0100

    Remove object, it was crap and unnecessary

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -1,3 +1,23 @@
+var modal = document.getElementsByClassName('modal');
+var addPlayer = document.getElementById('player');
+var addRule = document.getElementById('rule');
+var close = document.getElementsByClassName('close');
+
+addPlayer.onclick = function() {
+  modal[0].style.display = 'block';
+}
+
+addRule.onclick = function() {
+  modal[1].style.display = 'block';
+}
+
+for(btn of close) {
+  btn.onclick = function() {
+    this.parentElement.parentElement.style.display = 'none';
+  }
+}
+
+
 class Database {
   constructor(apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId, appId) {
     this.apiKey = apiKey;
@@ -25,119 +45,7 @@ class Database {
     return firebase.firestore().collection(collection).doc(localStorage.getItem('user'));
   }
 
-  add(collection, item) {
-    var newPlayer = document.getElementById('updateScores');
-    var data = this.init(collection);
-
-    updateScores.innerHTML += '<li><input type="text" id="newPlayerName"><button>+</button></li>';
-
-    //some validation
-    if(item != '') {
-      data.update({
-            [`${item}`]: 0
-      }).then(function(){
-        console.log('data added successfully!');
-        showScores();
-      }).catch(function(error){
-        console.log('error writing data');
-      })
-    } else {
-      console.log('Please enter a value');
-    }
-
-  }
-
-  update(collection, item, value) {
-    var data = this.init(collection);
-    var score = Number(value) //Prev: button.previousElementSibling.value;
-
-    data.get().then(function(doc){
-      if(doc.exists) {
-        var obj = doc.data();
-        var currentScore = obj[item];
-        newScore =  Number(currentScore) + score;
-        data.update({
-            [`${item}`]: `${newScore}`
-        });
-        showScores(localStorage.getItem('user'));
-      } else {
-        return 'no such document!';
-      }
-    }).catch(function(error){
-      return 'There was an error: ' + error;
-    });
-  }
-
-  remove(collection, item) {
-    var data = this.init(collection);
-    data.update({
-      [`${item}`]: firebase.firestore.FieldValue.delete()
-    }).then(function(){
-      console.log('Player deleted');
-      dsplay(collection);
-    }).catch(function(error){
-      console.log(error);
-    })
-  }
-
-  display(collection) {
-    // clear all displays
-    var display = document.getElementsByClassName('display');
-    for (var i = 0; i < display.length; i++) {
-      display[i].innerHTML = '';
-    }
-
-    var data = this.init(collection);
-    var scoresDiv = document.getElementById('scores');
-    var updateScores = document.getElementById('updateScores');
-    var addPlayerMsg = document.getElementById('addPlayersMsg');
-    data.get().then(function(doc){
-
-      //1. check if document exists
-      if(doc.exists) {
-
-        //1a. if document exists, check if it contains any data.  If so, output data
-        if(Object.keys(doc.data()).length > 0) {
-          //get player data
-          var obj = doc.data();
-
-          //loop through database object and output li
-          for(var key in obj) {
-            var name = key;
-            var score = obj[key];
-
-            //update page elements with db data
-            if(collection === 'teams') {
-              document.getElementById('updateScores').innerHTML += '<li class="player"><label for=' + name + '">'+ name + '</label><input class="input" type="number" id="' + name + '"><button class="button-primary" onclick="database.update(\'teams\', this.previousElementSibling.getAttribute(\'Id\'), this.previousElementSibling.value)">Winner!</button><button class="smallBtn" onclick="database.remove(\'teams\',this.previousElementSibling.previousElementSibling.getAttribute(\'id\'))">x</button></li>';
-              document.getElementById('scores').innerHTML += '<li class="player">' + name + ': ' + score + '</li>';
-            } else if (collection === 'rules') {
-                document.getElementsByClassName('houseRules')[0].innerHTML += '<li id="' + ruleNum + '">' + rule + '<button onclick="removeRule(this.parentElement.getAttribute(\'id\'))">x</button></li>';
-            }
-          };
-        } else {
-          //1b. if document exists but does not contain any data, clear page and prompt user to add data
-          if(addPlayerMsg != null && collection === 'teams') {
-            addPlayerMsg.innerHTML = '<p>Add a player to get started</p>';
-          }
-        }
-      } else {
-        //2. if document does not exist, prompt user to create a new document
-        if(addPlayerMsg != null && collection === 'teams') {
-          addPlayerMsg.innerHTML = '<p>No team here!  Please <a href="#" onclick="window.location.href=\'/create/\'">add a team</a> to get started</p>';
-        }
-      }
-    }).catch(function(error){
-      console.log(error);
-    });
-    var usr = localStorage.getItem('userName');
-    if(usr != null) {
-      document.getElementById('welcome').innerHTML = '<small>' + sessionStorage.getItem('greeting') + ' <a onclick="logout()" href="#">logout?</a></small>';
-    }
-  }
-
 }
-
-//firebase.initializeApp(firebaseConfig);
 
 var database = new Database(config.apiKey, config.authDomain, config.databaseURL, config.projectId,
   config.storageBucket, config.messagingSenderId, config.appId);
@@ -148,114 +56,8 @@ var greetings = ['Aloha', 'Yo', 'Hey', 'Hello', 'Welcome', 'Hi', 'Word up', 'Wha
     newScore,
     usr,
     name,
-    //db = firebase.firestore(),
 
-//Initialize auth ui
-
-    //ui = new firebaseui.auth.AuthUI(firebase.auth()),
-    provider = new firebase.auth.GoogleAuthProvider();
-
-console.log(greetings);
-
-function databaseOp(col) {
-  return database.init(col);
-}
-
-function getPlayer() {
-  var newPlayer = document.getElementById('updateScores');
-  newPlayer.innerHTML += '<li><input type="text" id="newPlayerName"><button onclick="database.add(\'teams\', this.previousElementSibling.value); this.parentElement.remove()">+</button></li>';
-}
-
-function sort(){
-  var data = databaseOp('teams');
-  var obj = data.get().then(function(doc){
-    if(doc.exists){
-
-    } else {
-      console.log('no such document');
-    }
-  })
-}
-
-function addRule(rule) {
-  var rulesDiv = document.getElementsByClassName('houseRules')[0];
-  var ruleNum;
-  var data = databaseOp('rules');
-  data.get().then(function(doc){
-    if(doc.exists) {
-      //count fields
-      ruleNum = 'rule' + (Object.keys(doc.data()).length === 0 ? 1 : Object.keys(doc.data()).length + 1);
-      data.update({
-        [`${ruleNum}`] : rule
-      });
-    } else {
-      data.set({
-        [`${ruleNum}`] : rule
-      });
-
-    }
-    rulesDiv.innerHTML += '<li id="' + ruleNum + '">' + rule + '<button onclick="removeRule(this.parentElement.getAttribute(\'id\'))">x</button></li>';
-
-  })
-}
-
-function removeRule(rule) {
-  var data = databaseOp('rules');
-  data.update({
-    [`${rule}`]: firebase.firestore.FieldValue.delete()
-  }).then(function(){
-    console.log('Rule deleted');
-    showRules();
-  }).catch(function(error){
-    console.log(error);
-  })
-}
-
-function showRules() {
-    var rules = document.getElementsByClassName('houseRules')[0];
-    var data = databaseOp('rules');
-
-    rules.innerHTML = '';
-    data.get().then(function(doc){
-      var obj = doc.data();
-      if(doc.exists) {
-        for(key in obj) {
-          if(window.location.pathname === '/main/') {
-            rules.innerHTML += '<li class="rule">' + obj[key] + '</li>';
-
-          } else {
-            rules.innerHTML += '<li id="' + key + '">' + obj[key] + '</li><button onclick="">x</button>';
-          }
-        }
-      } else  {
-
-      }
-    });
-}
-
-function newTeam(player) {
-  //take to new page or modal
-  //check whether doc exists
-  var data = databaseOp('teams');
-  var name = player.previousElementSibling.value;
-  var parent = document.getElementById('newPlayers');
-  var teamPlayers = document.getElementById('teamPlayers');
-
-  data.get().then(function(doc){
-    if(doc.exists) {
-      data.update({
-        [`${name}`] : 0
-      })
-    } else {
-      data.set({
-        [`${name}`] : 0
-      })
-    }
-  });
-
-  teamPlayers.innerHTML += '<li class="newPlayer" id="' + name + '">' + name + '<button onclick="removePlayer(this.parentElement.getAttribute(\'id\')); this.parentElement.remove()">x</button></li>';
-  teamPlayers.value = '';
-}
+provider = new firebase.auth.GoogleAuthProvider();
 
 async function login() {
   await firebase.auth().signInWithPopup(provider).then(function(result){
@@ -273,13 +75,102 @@ async function login() {
 
 }
 
-//// TODO: think about how to run this function when page loads?  Maybe put values into sessionStorage?
-function showScores() {
-  var players = databaseOp('teams');
+function add(collection, item) {
+
+      if(collection === 'teams') {
+        var newPlayer = document.getElementById('updateScores');
+        var data = database.init(collection);
+        //some validation
+        if(item != '') {
+          data.update({
+                [`${item}`]: 0
+          }).then(function(){
+            console.log('data added successfully!');
+            updateScores.innerHTML += '<li class="player"><label for=' + `${item}` + '">'+ `${item}` + '</label><input class="input" type="number" id="' + `${item}` + '"><button class="button-primary" onclick="update(\'teams\', this.previousElementSibling.getAttribute(\'Id\'), this.previousElementSibling.value)">Winner!</button><button class="smallBtn" onclick="remove(\'teams\',this.previousElementSibling.previousElementSibling.getAttribute(\'id\'))">x</button></li>';
+            document.getElementById('scores').innerHTML += '<li>' + `${item}` + ': 0</li>';
+          }).catch(function(error){
+            console.log('error writing data');
+          })
+        } else {
+          console.log('Please enter a value');
+        }
+
+      } else if (collection === 'rules') {
+        //If adding a rule do something else
+        var rulesDiv = document.getElementsByClassName('houseRules')[0];
+        var ruleNum;
+        var data = database.init('rules');
+        data.get().then(function(doc){
+          if(doc.exists) {
+            //count fields
+            ruleNum = 'rule' + (Object.keys(doc.data()).length === 0 ? 1 : Object.keys(doc.data()).length + 1);
+            data.update({
+              [`${ruleNum}`] : item
+            });
+          } else {
+            data.set({
+              [`${ruleNum}`] : item
+            });
+
+          }
+          rulesDiv.innerHTML += '<li id="' + ruleNum + '">' + item + '<button onclick="remove(\'rules\',this.parentElement.getAttribute(\'id\'))">x</button></li>';
+
+        })
+      }
+}
+
+function remove(collection, item) {
+  var data = database.init(collection);
+  data.update({
+    [`${item}`]: firebase.firestore.FieldValue.delete()
+  }).then(function(){
+    console.log('Item removed');
+
+  }).catch(function(error){
+    console.log(error);
+  });
+    display(collection);
+}
+
+function update(collection, item, value) {
+  var data = database.init(collection);
+  var score = Number(value) //Prev: button.previousElementSibling.value;
+
+  data.get().then(function(doc){
+    if(doc.exists) {
+      var obj = doc.data();
+      var currentScore = obj[item];
+      newScore =  Number(currentScore) + score;
+      data.update({
+          [`${item}`]: `${newScore}`
+      }).then(function(){
+        display(collection);
+      });
+    } else {
+      return 'no such document!';
+    }
+  }).catch(function(error){
+    return 'There was an error: ' + error;
+  });
+}
+
+function display(collection) {
+  // clear all displays
+  var display = document.getElementsByClassName('display');
+  /*for (var i = 0; i < display.length; i++) {
+    display[i].innerHTML = '';
+  }*/
+
+  var data = database.init(collection);
   var scoresDiv = document.getElementById('scores');
   var updateScores = document.getElementById('updateScores');
   var addPlayerMsg = document.getElementById('addPlayersMsg');
-  players.get().then(function(doc){
+  var display = document.getElementsByClassName(collection + '-display');
+  data.get().then(function(doc){
+
+    for (var i = 0; i < display.length; i++) {
+      display[i].innerHTML = '';
+    }
 
     //1. check if document exists
     if(doc.exists) {
@@ -289,31 +180,31 @@ function showScores() {
         //get player data
         var obj = doc.data();
 
-        // clear div contents
-        addPlayerMsg.innerHTML = '';
-        scoresDiv.innerHTML = '';
-        updateScores.innerHTML = '';
-
         //loop through database object and output li
         for(var key in obj) {
           var name = key;
           var score = obj[key];
 
           //update page elements with db data
-          updateScores.innerHTML += '<li class="player"><label for=' + name + '">'+ name + '</label><input class="input" type="number" id="' + name + '"><button class="button-primary" onclick="database.update(\'teams\', this.previousElementSibling.getAttribute(\'Id\'), this.previousElementSibling.value)">Winner!</button><button class="smallBtn" onclick="database.remove(\'teams\',this.previousElementSibling.previousElementSibling.getAttribute(\'id\'))">x</button></li>';
-          scoresDiv.innerHTML += '<li class="player">' + name + ': ' + score + '</li>';
+          if(collection === 'teams') {
+            document.getElementById('updateScores').innerHTML += '<li class="player"><label for=' + name + '">'+ name + '</label><input class="input" type="number" id="' + name + '"><button class="button-primary" onclick="update(\'teams\', this.previousElementSibling.getAttribute(\'Id\'), this.previousElementSibling.value)">Winner!</button><button class="smallBtn" onclick="remove(\'teams\',this.previousElementSibling.previousElementSibling.getAttribute(\'id\'))">x</button></li>';
+            document.getElementById('scores').innerHTML += '<li class="player">' + name + ': ' + score + '</li>';
+          } else if (collection === 'rules') {
+              for(var i = 0; i < display.length; i++) {
+                display[i].innerHTML = ''
+              }
+              document.getElementsByClassName('houseRules')[0].innerHTML += '<li id="' + key + '">' + obj[key] + '<button onclick="remove(\'rules\',this.parentElement.getAttribute(\'id\'))">x</button></li>';
+          }
         };
       } else {
         //1b. if document exists but does not contain any data, clear page and prompt user to add data
-        scoresDiv.innerHTML = '';
-        updateScores.innerHTML = '';
-        if(addPlayerMsg != null) {
+        if(addPlayerMsg != null && collection === 'teams') {
           addPlayerMsg.innerHTML = '<p>Add a player to get started</p>';
         }
       }
     } else {
       //2. if document does not exist, prompt user to create a new document
-      if(addPlayerMsg != null) {
+      if(addPlayerMsg != null && collection === 'teams') {
         addPlayerMsg.innerHTML = '<p>No team here!  Please <a href="#" onclick="window.location.href=\'/create/\'">add a team</a> to get started</p>';
       }
     }
--- main.md
+++ main.md
@@ -5,25 +5,36 @@ eleventyNavigation:
   order: 1
 templateClass: tmpl-main
 ---
-<div class="row">
-<div class="seven columns">
+<div class="grid">
+  <div class="modal">
+    <div class="modalContent">
+      <span class="close">&times;</span>
+      <input type="text" id="newPlayer" placeholder="New player name"><button onclick="add('teams', this.previousElementSibling.value); this.parentElement.parentElement.style.display='none'">+</button>
+    </div>
+  </div>
+  <div class="modal">
+    <div class="modalContent">
+      <span class="close">&times;</span>
+      <input type="text" id="newRule" placeholder="New rule"><button onclick="add('rules', this.previousElementSibling.value); this.parentElement.parentElement.style.display='none'">+</button>
+    </div>
+  </div>
+<div class="grid__col grid__col--3-of-5">
 <h2>Score</h2>
-  <ul id="updateScores">
+  <ul id="updateScores" class="teams-display">
   </ul>
-  <div id="addPlayersMsg" clas=="display"></div>
+  <div id="addPlayersMsg" class="display"></div>
   <ul>
   </ul>
-  <button class="button-primary" onclick="getPlayer()">Add Player</button>
 </div>
-<div class="five columns">
+<div class="grid__col grid__col--2-of-5">
 <h2>Leaderboard</h2>
   <div id="scoreContainer">
-      <ul id="scores" class="display">
+      <ul id="scores" class="teams-display">
       </ul>
   </div>
 <h2>House Rules</h2>
   <div id="rulesContainer">
-    <ol class="houseRules display">
+    <ol class="houseRules rules-display">
     </ol>
   </div>
 </div>
