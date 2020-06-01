28956a2b641e42ab30bea26cceac36fad1dd355f
Isham Jassat <isham.jassat@gmail.com>
2020-06-01 10:27:23 +0100

    Update Netlify function to output object

--- functions/token-hider.js
+++ functions/token-hider.js
@@ -9,15 +9,15 @@ exports.handler = async function(event, context, callback) {
 
   callback(null, {
     statusCode: 200,
-    body: [{
-      apiKey: apiKey,
-      authDomain: authDomain,
-      databaseURL: databaseURL,
-      projectId: projectId,
-      storageBucket: storageBucket,
-      messagingSenderId: messagingSenderId,
-      appId: appId
-    }]
+    body: {
+      'apiKey': apiKey,
+      'authDomain': authDomain,
+      'databaseURL': databaseURL,
+      'projectId': projectId,
+      'storageBucket': storageBucket,
+      'messagingSenderId': messagingSenderId,
+      'appId': appId
+    }
   });
 
 }
