8c640f28979e01b3dc2bfe01c20255f44a70330b
Isham Jassat <isham.jassat@gmail.com>
2020-06-01 10:49:01 +0100

    Test 17

--- functions/token-hider.js
+++ functions/token-hider.js
@@ -6,6 +6,7 @@ exports.handler = async function(event, context, callback) {
                 'storageBucket':process.env.STORAGE_BUCKET,
                 'messagingSenderId':process.env.MESSAGING_SENDER_ID,
                 'appId':process.env.APP_ID}
+  console.log(body)
 
   callback(null, {
     statusCode: 200,
