a2aebc1894048d9cbb6bfd6efb631917ac785a64
Isham Jassat <isham.jassat@gmail.com>
2020-06-01 10:46:28 +0100

    Create body object in netlify function token-hider

--- functions/token-hider.js
+++ functions/token-hider.js
@@ -1,23 +1,15 @@
 exports.handler = async function(event, context, callback) {
-  const apiKey = process.env.APP_KEY,
-        authDomain = process.env.AUTH_DOMAIN,
-        databaseURL = process.env.DB_URL,
-        projectId = process.env.PROJECT_ID,
-        storageBucket = process.env.STORAGE_BUCKET,
-        messagingSenderId = process.env.MESSAGING_SENDER_ID,
-        appId = process.env.APP_ID
+  const body = {'apiKey':process.env.APP_KEY,
+                'authDomain':process.env.AUTH_DOMAIN,
+                'databaseURL':process.env.DB_URL,
+                'projectId':process.env.PROJECT_ID,
+                'storageBucket':process.env.STORAGE_BUCKET,
+                'messagingSenderId':process.env.MESSAGING_SENDER_ID,
+                'appId':process.env.APP_ID}
 
   callback(null, {
     statusCode: 200,
-    body: [
-      {'apiKey': apiKey},
-      {'authDomain': authDomain},
-      {'databaseURL': databaseURL},
-      {'projectId': projectId},
-      {'storageBucket': storageBucket},
-      {'messagingSenderId': messagingSenderId},
-      {'appId': appId}
-    ]
+    body: body
   });
 
 }
